53b2e2d342b2b83d9b6483952dc916dea3db09c2
yorickburgers <yorickburgers@live.nl>
2025-05-25 14:23:26 +0200

    base Springboot

---
+++ src/test/java/nl/novi/bachwtechiteasy/BaChwTechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.bachwtechiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaChwTechItEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
