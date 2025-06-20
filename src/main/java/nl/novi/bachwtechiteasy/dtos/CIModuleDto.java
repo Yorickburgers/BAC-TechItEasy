1a810181c3fff53f67a8f9de9981eb0f2cbe3fd1
yorickburgers <yorickburgers@live.nl>
2025-06-20 16:46:26 +0200

    made dtos for new classes

---
+++ src/main/java/nl/novi/bachwtechiteasy/dtos/CIModuleDto.java
@@ -0,0 +1,8 @@
+package nl.novi.bachwtechiteasy.dtos;
+
+public class CIModuleDto {
+    public Long id;
+    public String name;
+    public String type;
+    public Double price;
+}
---
+++ src/main/java/nl/novi/bachwtechiteasy/dtos/CIModuleInputDto.java
@@ -0,0 +1,8 @@
+package nl.novi.bachwtechiteasy.dtos;
+
+public class CIModuleInputDto {
+    //public Long id;
+    public String name;
+    public String type;
+    public Double price;
+}
