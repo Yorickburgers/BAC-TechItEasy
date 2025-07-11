0079f540312351eda0bf1ec61224e83cfde4d2bc
yorickburgers <yorickburgers@live.nl>
2025-07-11 12:21:17 +0200

    added UserService

---
+++ src/main/java/nl/novi/bachwtechiteasy/services/UserService.java
@@ -0,0 +1,80 @@
+package nl.novi.bachwtechiteasy.services;
+
+import nl.novi.bachwtechiteasy.dtos.UserDto;
+import nl.novi.bachwtechiteasy.exceptions.RecordNotFoundException;
+import nl.novi.bachwtechiteasy.models.Authority;
+import nl.novi.bachwtechiteasy.models.User;
+import nl.novi.bachwtechiteasy.repositories.UserRepository;
+import nl.novi.bachwtechiteasy.utils.RandomStringGenerator;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserDto> getUsers() {
+        List<UserDto> users = new ArrayList<>();
+        userRepository.findAll().forEach(user -> {
+            users.add(UserMapper.toUserDto(user));
+        });
+        return users;
+    }
+
+    public UserDto getUser(String username) {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new RecordNotFoundException("Can't find user " + username));
+        return UserMapper.toUserDto(user);
+    }
+
+    public boolean userExists(String username) {
+        return userRepository.existsById(username);
+    }
+
+    public String createUser(UserDto dto) {
+        String randomString = RandomStringGenerator.generateAlphaNumeric(20);
+        dto.apikey = randomString;
+        User newUser = userRepository.save(UserMapper.toUser(dto));
+        return newUser.getUsername();
+    }
+
+    public void deleteUser(String username) {
+        userRepository.deleteById(username);
+    }
+
+    public void updateUser(String username, UserDto newUser) {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new RecordNotFoundException("Can't find user " + username));
+        user.setPassword(newUser.password);
+        userRepository.save(user);
+    }
+
+    public Set<Authority> getAuthorities(String username) {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new RecordNotFoundException("Can't find user " + username));
+        UserDto dto = UserMapper.toUserDto(user);
+        return dto.authorities;
+    }
+
+    public void addAuthority(String username, String authority) {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new RecordNotFoundException("Can't find user " + username));
+        user.addAuthority(new Authority(username, authority));
+        userRepository.save(user);
+    }
+
+    public void removeAuthority(String username, String authority) {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new RecordNotFoundException("Can't find user " + username));
+        Authority authorityToRemove = user.getAuthorities().stream().filter((a) -> a.getAuthority().equalsIgnoreCase(authority)).findAny().get();
+        user.removeAuthority(authorityToRemove);
+        userRepository.save(user);
+    }
+}
